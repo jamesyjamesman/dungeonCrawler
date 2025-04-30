bcb99c9aec0c9d270cdabb735b42829574e37bf1
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-30 16:56:35 -0500

    Creates a new kind of enemy: MageEnemy.

---
+++ src/MageEnemy.java
@@ -0,0 +1,9 @@
+public class MageEnemy extends Enemy {
+    int maxMana;
+    int currentMana;
+    public MageEnemy() {
+        this.damageType = "magic";
+        this.maxMana = 0;
+        this.currentMana = 0;
+    }
+}
