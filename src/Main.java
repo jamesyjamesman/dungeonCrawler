254d8eb2bcc1e57fde6aa2bdbebab3e6c1719ac5
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-29 23:50:56 -0500

    Adds a few lines to see how the flow of the game could work.

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,24 @@ public class Main {
         Scanner lineScanner = new Scanner(System.in);
         String playerName = lineScanner.nextLine();
         Player newPlayer = new Player(playerName);
-        System.out.println("Your character's name is:" + newPlayer.name);
+        System.out.println("Your character's name is: " + newPlayer.name);
+
+        Room newRoom = new Room();
+        newRoom.description = "Coolbeans";
+        System.out.println("Room description: " + newRoom.description);
+
+        TrapRoom newTrapRoom = new TrapRoom();
+        newTrapRoom.description = "Oh no! There's a bunch of stalagmites that fall from the ceiling when you enter!";
+        System.out.println("Would you like to go into the next room? (y/n)");
+        String answer = lineScanner.nextLine();
+        if (answer.equals("y")) {
+            System.out.println(newTrapRoom.description);
+            System.out.println("You took " + newTrapRoom.damageDealt + " points of damage!");
+            newPlayer.currentHealth -= newTrapRoom.damageDealt;
+            System.out.println("You now have " + newPlayer.currentHealth + " health!");
+        } else {
+            System.out.println("Boring!");
+        }
+
     }
 }
