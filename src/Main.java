2f294df1ba2edf8ee1353ba7f333beb3356b60bd
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-30 16:56:10 -0500

    Imports arraylists. changes the test scenario to have two options.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,20 +9,27 @@ public class Main {
         Player newPlayer = new Player(playerName);
         System.out.println("Your character's name is: " + newPlayer.name);
 
-        TrapRoom currentRoom = RoomInit.trapRoomInit();
-        
-        System.out.println("You find yourself in a cave. There's only one tunnel that leads anywhere.");
-        System.out.println(currentRoom.appearance);
-        System.out.println("Would you like to go into the next room? (y/n)");
+        ArrayList<TrapRoom> trapRooms = RoomInit.trapRoomInit();
+        TrapRoom firstOption = trapRooms.get(0);
+
+        ArrayList<EnemyRoom> enemyRooms = RoomInit.enemyRoomInit();
+        EnemyRoom secondOption = enemyRooms.get(0);
+
+        System.out.println("You find yourself in a cave. There's two tunnels.");
+        System.out.println("For the first option, " + firstOption.appearance);
+        System.out.println("For the second option, " + secondOption.appearance);
+        System.out.println("Would you like to go into the first room? (y/n)");
 
         String answer = lineScanner.nextLine();
         if (answer.equals("y")) {
-            System.out.println(currentRoom.description);
-            System.out.println("You took " + currentRoom.damageDealt + " points of damage!");
-            newPlayer.currentHealth -= currentRoom.damageDealt;
+            System.out.println(firstOption.description);
+            System.out.println("You took " + firstOption.damageDealt + " points of damage!");
+            newPlayer.currentHealth -= firstOption.damageDealt;
             System.out.println("You now have " + newPlayer.currentHealth + " health!");
         } else {
-            System.out.println("Boring!");
+            System.out.println(secondOption.description);
+            System.out.println(secondOption.battleInitiationMessage);
+            System.out.println("You took " + secondOption.enemies.get(0).damage + " points of damage!");
         }
 
     }
