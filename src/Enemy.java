227bd866911e614d040b7311754d1984723eebe6
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-30 16:57:58 -0500

    Enemy class. contains some dummy values.

---
+++ src/Enemy.java
@@ -0,0 +1,16 @@
+public class Enemy {
+    int health;
+    int damage;
+    int armor;
+    int evasiveness;
+    String species;
+    String damageType;
+    public Enemy() {
+        this.health = 0;
+        this.damage = 0;
+        this.armor = 0;
+        this.evasiveness = 0;
+        this.species = "";
+        this.damageType = "";
+    }
+}
