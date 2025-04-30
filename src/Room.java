e82bfdfb6d962f45daccad7894cea0cb7ec25ec2
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-04-30 00:12:06 -0500

    Added a few more characteristics to the class

--- src/Room.java
+++ src/Room.java
@@ -1,6 +1,10 @@
 public class Room {
+    String appearance;
     String description;
+    int id;
     public Room() {
+        this.appearance = "";
         this.description = "";
+        this.id = 0;
     }
 }
