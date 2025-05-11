b1f77515afb0306d01586308c710e2495343df87
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-11 02:35:18 -0500

    Allows for items to manipulate the maximum health of the player.

--- src/ItemRoom.java
+++ src/ItemRoom.java
@@ -5,11 +5,13 @@ public class ItemRoom extends Room {
     String itemName;
     int healthRestored;
     int[] restorationRange;
+    int maxHealthChange;
     public ItemRoom() {
         super();
         this.itemName = "";
         this.healthRestored = 0;
         this.restorationRange = new int[2];
+        this.maxHealthChange = 0;
     }
 
     @Override
@@ -31,6 +33,11 @@ public class ItemRoom extends Room {
             System.out.println("Yuck! The " + itemName + " was terrible... You lost " + this.healthRestored * -1 + " health.");
             player.takeDamage(this.healthRestored * -1);
         } else {
+            if (this.maxHealthChange != 0) {
+                System.out.println("Wow! A surge of power courses through you... your maximum health has increased by " + this.maxHealthChange + "!");
+                player.maxHealth += this.maxHealthChange;
+                player.currentHealth += this.maxHealthChange;
+            }
             player.heal(this.healthRestored);
             System.out.println("Yum! That " + itemName + " was great! You replenished " + this.healthRestored + " health.");
         }
