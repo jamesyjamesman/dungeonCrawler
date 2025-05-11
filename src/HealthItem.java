2a64f95f412ffd0aade32e01d59c40b2371134b3
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-11 16:49:20 -0500

    Adds the health item class, which contains all of the current items. Most of the code is moved over from ItemRoom, to properly implement item objects.

---
+++ src/HealthItem.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class HealthItem extends Item{
+    int healthRestored;
+    int[] restorationRange;
+    int maxHealthChange;
+    HealthItem() {
+        this.healthRestored = 0;
+        this.restorationRange = new int[2];
+        this.maxHealthChange = 0;
+    }
+
+    @Override
+    public void useItem(Player player) {
+        this.healthRestored = new Random().nextInt(this.restorationRange[0], this.restorationRange[1]);
+
+        if (this.healthRestored < 0) {
+            System.out.println("Yuck! The " + this.name + " was terrible... You lost " + this.healthRestored * -1 + " health.");
+            player.takeDamage(this.healthRestored * -1);
+        } else {
+            if (this.maxHealthChange != 0) {
+                System.out.println("Wow! A surge of power courses through you... your maximum health has increased by " + this.maxHealthChange + "!");
+                player.changeMaxHealth(this.maxHealthChange);
+            }
+            player.heal(this.healthRestored);
+            System.out.println("Yum! That " + this.name + " was great! You replenished " + this.healthRestored + " health.");
+        }
+        player.inventory.remove(this);
+    }
+}
