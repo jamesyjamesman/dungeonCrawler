80be751c16a312be724763eade0fc760275aae58
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-11 02:13:46 -0500

    Adds methods to heal and take damage. If health falls below zero, it calls the death sequence method.

--- src/Player.java
+++ src/Player.java
@@ -8,9 +8,22 @@ public class Player {
         this.currentHealth = 20;
     }
 
-    public void checkStatus(Player player) {
+    public void checkStatus() {
         System.out.println("Current player status:");
         //can this just be this.currentHealth and then I can avoid having to pass the object to itself?
-        System.out.println("Health: " + player.currentHealth + "/" + player.maxHealth);
+        System.out.println("Health: " + this.currentHealth + "/" + this.maxHealth);
+    }
+    public void takeDamage(int damage) {
+        this.currentHealth -= damage;
+        if (this.currentHealth < 0) {
+            this.currentHealth = 0;
+            Main.doDeathSequence(this);
+        }
+    }
+    public void heal(int health) {
+        this.currentHealth += health;
+        if (this.currentHealth > this.maxHealth) {
+            this.currentHealth = this.maxHealth;
+        }
     }
 }
