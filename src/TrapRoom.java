917e07f6dda3f1e0aaa38eb2a4261e81c5f142f2
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-11 02:14:20 -0500

    Calls the take damage method instead of changing the value directly.

--- src/TrapRoom.java
+++ src/TrapRoom.java
@@ -9,6 +9,6 @@ public class TrapRoom extends Room {
     public void completeRoomActions(Player player) {
         super.completeRoomActions(player);
         System.out.println("You took " + this.damageDealt + " damage!");
-        player.currentHealth -= this.damageDealt;
+        player.takeDamage(this.damageDealt);
     }
 }
