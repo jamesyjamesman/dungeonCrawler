0fbcced9bb0b068e1e8241879ea64c3808440486
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-11 16:54:50 -0500

    Adds an inventory to the player. Adds a function to add an item to their inventory, as well as check the contents. Users can use items from their inventory.

--- src/Player.java
+++ src/Player.java
@@ -1,16 +1,47 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Player {
     String name;
     int maxHealth;
     int currentHealth;
+    ArrayList<Item> inventory;
     public Player(String newName) {
         this.name = newName;
         this.maxHealth = 20;
         this.currentHealth = 20;
+        this.inventory = new ArrayList<>();
     }
 
+    public void addItemToInventory(Item item) {
+        this.inventory.add(item);
+    }
+
+    public void checkInventory() {
+
+        if (this.inventory.isEmpty()) {
+            System.out.println("Your inventory is empty!");
+            return;
+        }
+
+        Scanner lineScanner = new Scanner(System.in);
+        for (int i = 0; i < this.inventory.size(); i++) {
+            System.out.println((i+1) + ". " + this.inventory.get(i).name + ": " + this.inventory.get(i).description);
+        }
+
+        System.out.println("Would you like to use an item? Input the appropriate number to use it, or exit to return to gameplay.");
+        String response = Main.inputHelper(this, lineScanner.nextLine());
+
+        if (response.equals("exit")) {
+            System.out.println("You exit your inventory.");
+            return;
+        }
+
+        inventory.get(Integer.parseInt(response) - 1).useItem(this);
+
+    }
     public void checkStatus() {
         System.out.println("Current player status:");
-        //can this just be this.currentHealth and then I can avoid having to pass the object to itself?
         System.out.println("Health: " + this.currentHealth + "/" + this.maxHealth);
     }
     public void takeDamage(int damage) {
