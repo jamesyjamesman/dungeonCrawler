b71d882caf2aae8a7b1ca3ec3bc6672b475e4492
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-11 01:17:29 -0500

    Moved checkStatus function into the Player class

--- src/Player.java
+++ src/Player.java
@@ -7,4 +7,10 @@ public class Player {
         this.maxHealth = 20;
         this.currentHealth = 20;
     }
+
+    public void checkStatus(Player player) {
+        System.out.println("Current player status:");
+        //can this just be this.currentHealth and then I can avoid having to pass the object to itself?
+        System.out.println("Health: " + player.currentHealth + "/" + player.maxHealth);
+    }
 }
