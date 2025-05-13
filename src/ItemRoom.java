d105a9f57826983949e4c57672c24acfd0fc086f
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-13 18:45:41 -0500

    Allows the user to equip relics as soon as they are obtained

--- src/ItemRoom.java
+++ src/ItemRoom.java
@@ -18,8 +18,19 @@ public class ItemRoom extends Room {
             System.out.println("You chose to forgo the loot...");
             return;
         }
-        System.out.println("You stash the " + this.item.name + " in your bag.");
+
         player.addItemToInventory(this.item);
+
+        if (this.item instanceof Relic) {
+            System.out.println("Would you like to equip this now? (y/n)");
+            response = Main.inputHelper(player, lineScanner.nextLine());
+            if (response.equals("yes") || response.equals("y")) {
+                this.item.useItem(player);
+                System.out.println("The " + this.item.name + " has been equipped!");
+                return;
+            }
+        }
+        System.out.println("You stash the " + this.item.name + " in your bag.");
     }
     public void changeItem(Item item) {
         this.item = item;
