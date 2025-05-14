3d149fbeadb7f3279af8b6b7612f2438338b36f2
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-13 21:36:48 -0500

    Implements checking for curses

--- src/ItemRoom.java
+++ src/ItemRoom.java
@@ -13,6 +13,11 @@ public class ItemRoom extends Room {
 
         Scanner lineScanner = new Scanner(System.in);
         System.out.println("Would you like to take the " + this.item.name + "? (y/n)");
+        if (player.equippedRelicIndex("Relic of Curse Detection") > -1) {
+            if (this.item instanceof Relic checkRelic && checkRelic.isCursed()) {
+                    System.out.println("Warning! The " + this.item.name + " is cursed!");
+            }
+        }
         String response = Main.inputHelper(player, lineScanner.nextLine());
         if (response.equals("no") || response.equals("n")) {
             System.out.println("You chose to forgo the loot...");
