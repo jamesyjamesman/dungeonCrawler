ea32b96a9a7cb95b00a06f79eee44fe6a588b7e2
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-13 20:53:17 -0500

    Relics now have a 1/5 to be cursed, and cause damage whenever used. Currently, there is no way to remove or cleanse this.

--- src/Relic.java
+++ src/Relic.java
@@ -1,9 +1,19 @@
+import java.util.Random;
+
 public abstract class Relic extends Item{
     boolean equipped;
+    boolean cursed;
     public Relic(){
         this.equipped = false;
+        this.cursed = new Random().nextInt(5) == 0;
     }
-    public abstract void useRelic(Player player, Room room);
+    public void useRelic(Player player, Room room) {
+        if (this.cursed) {
+            int amountDamage = new Random().nextInt(5);
+            System.out.println("Your cursed " + this.name + " caused 1 damage!");
+            player.takeDamage(amountDamage);
+        }
+    }
 
     @Override
     public void useItem(Player player) {
@@ -16,9 +26,16 @@ public abstract class Relic extends Item{
         player.equippedRelics.add(this);
         int relicIndex = player.findItemInInventory(this);
         player.inventory.remove(relicIndex);
+        if (this.cursed) {
+            System.out.println("Oh no! the " + this.name + " was cursed!");
+        }
     }
 
     public void unequipRelic(Player player){
+        if (this.cursed) {
+            System.out.println("The relic is welded to you painfully. You can't remove it!");
+            return;
+        }
         this.equipped = false;
         player.equippedRelics.remove(this);
         player.addItemToInventory(this);
