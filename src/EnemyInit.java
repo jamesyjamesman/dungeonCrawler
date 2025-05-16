e702c24527de28e6e5fb86707fef32e1f8779c56
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-15 19:46:25 -0500

    Adds two new basic enemies

--- src/EnemyInit.java
+++ src/EnemyInit.java
@@ -1,16 +1,29 @@
 import java.util.ArrayList;
 
 public class EnemyInit {
-    static ArrayList<MageEnemy> MageEnemyList = new ArrayList<>();
-    public static ArrayList<MageEnemy> enemyInit() {
-        MageEnemy goblinMage = new MageEnemy();
-        goblinMage.currentHealth = 9;
-        goblinMage.currentMana = 100;
-        goblinMage.maxMana = 100;
-        goblinMage.damageType = "magic";
-        goblinMage.species = "goblin";
-        goblinMage.damage = 2;
-        MageEnemyList.add(goblinMage);
-        return MageEnemyList;
+    static ArrayList<Enemy> enemyList = new ArrayList<>();
+    public static ArrayList<Enemy> enemyInit() {
+            MageEnemy goblinMage = new MageEnemy();
+            goblinMage.currentHealth = 7;
+            goblinMage.currentMana = 100;
+            goblinMage.maxMana = 100;
+            goblinMage.damageType = "magic";
+            goblinMage.species = "goblin";
+            goblinMage.damage = 2;
+            enemyList.add(goblinMage);
+
+            Enemy goblin = new Enemy();
+            goblin.currentHealth = 8;
+            goblin.species = "goblin";
+            goblin.damage = 1;
+            enemyList.add(goblin);
+
+            Enemy orc = new Enemy();
+            orc.currentHealth = 12;
+            orc.species = "orc";
+            orc.damage = 2;
+            enemyList.add(orc);
+
+        return enemyList;
     }
 }
