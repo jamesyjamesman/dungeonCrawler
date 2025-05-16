d2a8832b204516199ec9fefb2ab76a3292f172ec
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-15 19:34:24 -0500

    Enemies can now attack and take damage.

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,16 +1,31 @@
 public class Enemy {
-    int health;
+    int currentHealth;
     int damage;
     int armor;
     int evasiveness;
     String species;
     String damageType;
     public Enemy() {
-        this.health = 0;
+        this.currentHealth = 0;
         this.damage = 0;
         this.armor = 0;
         this.evasiveness = 0;
         this.species = "";
         this.damageType = "";
+    }
+
+    public boolean takeDamage(int damage) {
+        this.currentHealth -= damage;
+        if (this.currentHealth < 0) {
+            this.currentHealth = 0;
+            return true;
+        }
+        return false;
+    }
+
+    public void attack(Player player) {
+        player.takeDamage(this.damage);
+        System.out.println("The " + this.species + " attacked you, dealing " + this.damage + " damage!");
+        //will get more complex with weapons, etc.
     }
 }
