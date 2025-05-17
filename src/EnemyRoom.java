bc31e55b517f0bfa84ba575970d485d4d5d978e9
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-16 19:10:38 -0500

    Properly resets enemies after they die.

--- src/EnemyRoom.java
+++ src/EnemyRoom.java
@@ -2,10 +2,12 @@ import java.util.ArrayList;
 
 public class EnemyRoom extends Room {
     ArrayList<Enemy> enemies;
+    ArrayList<Enemy> defeatedEnemies;
     String battleInitiationMessage;
     public EnemyRoom() {
         super();
         this.enemies = new ArrayList<>();
+        this.defeatedEnemies = new ArrayList<>();
         this.battleInitiationMessage = "";
     }
 
@@ -14,5 +16,12 @@ public class EnemyRoom extends Room {
         super.completeRoomActions(player);
         System.out.println(this.battleInitiationMessage);
         Battle.battleLoop(player, this);
+        for (int i = 0; i < this.defeatedEnemies.size(); i++) {
+            this.enemies.add(this.defeatedEnemies.get(i));
+            this.enemies.get(i).reset();
+        }
+        this.defeatedEnemies.clear();
     }
+
+
 }
