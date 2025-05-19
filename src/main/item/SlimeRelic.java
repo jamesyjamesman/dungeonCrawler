1235ddf9aec21fd411cf137c950a84eb5afc2c6a
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-19 10:36:38 -0500

    Adds a new relic that allows some attacks to bounce off of the player.

---
+++ src/main/item/SlimeRelic.java
@@ -0,0 +1,9 @@
+package main.item;
+
+//logic handled in Enemy.attack()
+public class SlimeRelic extends Relic {
+    public SlimeRelic() {
+        this.name = "Relic of Bounciness";
+        this.description = "This relic covers you in viscous, bouncy slime. Some attacks just bounce right off!";
+    }
+}
