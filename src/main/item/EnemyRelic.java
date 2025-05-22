3cd56fff7d5c1ffecf41d50b9cee018155b24a40
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-21 23:49:03 -0500

    Adds a new relic that tells the player how much health enemies have.

---
+++ src/main/item/EnemyRelic.java
@@ -0,0 +1,8 @@
+package main.item;
+
+public class EnemyRelic extends Relic {
+    public EnemyRelic() {
+        this.name = "Relic of Enemy Information";
+        this.description = "This relic allows you to detect some extra information about your enemies.";
+    }
+}
