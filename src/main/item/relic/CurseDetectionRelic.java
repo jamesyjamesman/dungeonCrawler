596bea8327a4dcffb3bced9f0b9e1f6d27451b69
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-25 16:28:43 -0500

    Renamed class for better clarity

--- src/main/item/relic/CurseDetectionRelic.java
+++ src/main/item/relic/CurseDetectionRelic.java
@@ -1,7 +1,7 @@
 package main.item.relic;
 
-public class CurseRelic extends Relic {
-    public CurseRelic() {
+public class CurseDetectionRelic extends Relic {
+    public CurseDetectionRelic() {
         setName("Relic of Curse Detection");
         setDescription("This relic allows you to sense the sinister energies that come off of cursed objects.");
     }
