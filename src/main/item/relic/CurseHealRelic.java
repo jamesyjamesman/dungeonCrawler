688073d6bd39ee10c7a2354482e0dcf086d12db4
jamesyjamesman <jameson.price@my.uwrf.edu>
2025-05-25 16:29:24 -0500

    Adds a new relic that makes cursed relics heal instead of doing damage.

---
+++ src/main/item/relic/CurseHealRelic.java
@@ -0,0 +1,9 @@
+package main.item.relic;
+
+public class CurseHealRelic extends Relic {
+    public CurseHealRelic() {
+        setName("Relic of Cursed Healing");
+        setDescription("This interesting relic inverts the twisted energies from cursed relics, healing you instead.");
+        setCursed(true);
+    }
+}
